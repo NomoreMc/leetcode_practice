744e09caca57a48896bc28c91c59b6ee67ec316c
nomore <nomoremc@163.com>
2023-01-04 22:37:50 +0800

    修改gencov脚本

--- gencov.sh
+++ gencov.sh
@@ -17,13 +17,18 @@ gen_coverage(){
 
     cd ${PROJ_ROOT_PATH}/report
 
-    lcov --rc lcov_branch_coverage=1 --capture --initial --directory ./ --base-directory ./ --output-file base.info
-    lcov --rc lcov_branch_coverage=1 --capture --directory ./ --test-name full --output-file test.info
+    lcov --rc lcov_branch_coverage=1 --capture --initial --directory ./ --output-file base.info
+    lcov --rc lcov_branch_coverage=1 --capture --base-directory ./ --directory ./ --output-file test.info --test-name full
 
-    genhtml base.info test.info
+    if [ -s test.info ]; then
+        lcov  -a base.info -a test.info -o main_test.info
+        genhtml main_test.info --branch-coverage --title "FULL Cov Test" >> gcov.txt
+    else
+        genhtml base.info --branch-coverage --title "FULL Cov Test" >> gcov.txt
+    fi
 
-    rm -rf *.gcno
-    rm -rf *.gcda
+    rm -f *.gcno
+    rm -f *.gcda
 }
 
 gen_coverage
